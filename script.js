b08b101f117de53765b6a92f37779419bc2eb35e
safetyWhat <137813061+safetyWhat@users.noreply.github.com>
2023-08-04 12:53:15 -0400

    Create Etch function.

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let squareNum = 1
 
 for (x = 0; x < 16; x++) {
     let row = 'row' + rowNum;
-    console.log(row);
+    //console.log(row);
     const rows = document.createElement('div');
     rows.setAttribute('id', row);
     rows.className = 'rows';
     container.appendChild(rows);
     
     for (i = 0; i < 16; i++) {
-        console.log(squareNum);
+        //console.log(squareNum);
         const squares = document.createElement('div');
         squares.className = 'squares';
         //squares.textContent = squareNum;
@@ -20,3 +20,9 @@ for (x = 0; x < 16; x++) {
     }
     rowNum++
 }
+let squares = document.querySelectorAll('.squares')
+squares.forEach(function(changeColor) {
+changeColor.addEventListener("mouseenter", (event) => {
+    event.target.style.background = "black";
+}, false);})
+
